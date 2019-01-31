8fbb20530ad0ab52ae91255acbebfa700ce28299
ahmdkhled <ahmdkhled@gmail.com>
2019-01-31 18:46:14 +0200

    add images of favorite products to response

--- favorite.php
+++ favorite.php
@@ -20,7 +20,24 @@ function getFavorites($userId){
   $result=array();
   if ($query->num_rows>0) {
     while ($row=$query->fetch_assoc()) {
-      $result[]=$row;
+      $temp = array();
+      $temp['product_id']=$row['id'];
+      $temp['product_name']=$row['name'];
+      $temp['product_price']=$row['price'];
+      $temp['product_quantity']=$row['quantity'];
+      $temp['product_description']=$row['description'];
+      $temp['product_marketId']=$row['marketId'];
+      $temp['product_categoryId']=$row['categoryId'];
+
+      $imagesQuery="select * from product_media where product_media.productId =".$row['id'] ;
+      $images=$dbconnect->query($imagesQuery);
+      while($imageRow = $images->fetch_assoc()){
+        $temp['media'][] = array(
+              'image_id' => $imageRow['media_id'],
+              'image_url' => $imageRow['media_url']
+        );
+      }
+      array_push($result, $temp);
     }
   }
   echo json_encode($result);
