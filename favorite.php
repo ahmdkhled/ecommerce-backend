3ab20c56b389ed35a3235c99ef0b1b83aa1957a7
ahmdkhled <ahmdkhled@gmail.com>
2019-02-01 18:06:45 +0200

    feat: add delete favorite product feature

--- favorite.php
+++ favorite.php
@@ -9,12 +9,22 @@ if ($_SERVER['REQUEST_METHOD']=='GET') {
      getFavorites($userId);
   }
 
+}elseif ($_SERVER['REQUEST_METHOD']=='DELETE') {
+  parse_str(file_get_contents('php://input'), $_DELETE);
+  if (isset($_DELETE['userId'],$_DELETE['productId'])) {
+    $productId=$_DELETE['productId'];
+    $userId=$_DELETE['userId'];
+     deleteProduct($productId,$userId);
+  }else{
+    http_response_code(404);
+
+  }
 }
 
 function getFavorites($userId){
   $querySql="SELECT * FROM favorite_products
   inner join products on favorite_products.product_id =products.id
-   where favorite_products.userId =$userId";
+  where favorite_products.userId =$userId";
   global $dbconnect;
   $query=$dbconnect->query($querySql);
   $result=array();
@@ -41,6 +51,19 @@ function getFavorites($userId){
     }
   }
   echo json_encode($result);
+
+}
+
+function deleteProduct($productId,$userId){
+  global $dbconnect;
+  $deleteSql="DELETE from favorite_products where userId=$userId and product_id=$productId";
+  if ($dbconnect->query($deleteSql)==true) {
+    http_response_code(200);
+  }else{
+    http_response_code(402);
+
+  }
+
 
 }
 
